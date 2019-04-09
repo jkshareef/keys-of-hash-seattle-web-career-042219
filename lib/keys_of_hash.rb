8930a435cc6f6ec4a899036493d4c3ef5c8d5150
Jamal Shareef <theepie@hotmail.com>
2019-04-09 01:10:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
 class Hash
-  def keys_of(*arguments)
+  def keys_of(*arguments, hash)
     array = Array.new
     # code goes here
     arguments.collect do |argument|
       hash.collect do |key, value|
+        binding.pry
         if value == argument
           array.push(key)
         end
